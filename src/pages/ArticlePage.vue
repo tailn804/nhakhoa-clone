bf6c8c5a4aa227967bee04856fb2ad31e19ad27c
hung158 <litruong158@gmail.com>
2024-10-20 09:04:24 -0700

    Add Image Class Page wiki

--- src/pages/ArticlePage.vue
+++ src/pages/ArticlePage.vue
@@ -24,29 +24,73 @@
             <div class="h-7"></div>
             <div class="px-3">
                 <div class=" my-4">
-                    <img class="w-auto h-[250px] bg-contain" src="https://lh3.googleusercontent.com/Q6LEGMhA19zCLbNry5vIE67pZq6WEmjhoGwKSq4e8QINJeSj9ZCeWvaBUHIMqQInIsDOO9ZXEngWMgZsfV7f4WofSQuUtXyMAji5xnGfNc55rBvjHvSI_QIRWDuCjLcoTg=w1280" alt="">
+                    <img class="w-auto h-[250px] bg-contain" src="https://lh5.googleusercontent.com/zWt9RgffQvFLz9HYg4Xr1PYLhGCCry6mrsaW0ayW5K9WotqvpWW9y4rKXp2-eedl7XxU1rC4DAeFjKgrBxL5E3ajLgUnhH7s3PVTM34T9SOWCwyYRDQrMTu0X142I54Uuw=w1280" alt="">
                 </div>
-                
-                <div class="px-2 py-3">
+                <div class="h-5"></div>
+                <div class="px-2 py-3 ">
                     <p class="text-[19px] font-bold block">
                         Purpose
                     </p>
-                    <span class="mt-[6px] block" >
-                        You can use spaces or group conversations in Google Chat to communicate with one or more people.
+                    <span class="mt-2 block text-xl font-thin" >
+                        You can use spaces or group conversations in Google Chat to communicate <br> with one or more people.
                     </span>
-                    <ul class="list-square pl-5">
-                        <li class="mt-[6px]" >
-                            Spaces: A central place where people can share files, assign tasks, and stay connected.
+                    <ul class="mt-[6px] list-square pl-5">
+                        <li class=" text-xl font-thin"  >
+                            Spaces: A central place where people can share files, assign tasks, and <br>stay connected.
                         </li>
-                        <li class="mt-[6px]" >
-                            Group conversations: Chat directly with a group. For example, use group conversations to have a quick discussion after a meeting.
+                        <li class=" text-xl font-thin" >
+                            Group conversations: Chat directly with a group. For example, use group <br> conversations to have a quick discussion after a meeting.
                         </li>
                     </ul>
-                    <span class="block" >
+                    <span class="mt-2 block text-xl font-thin" >
                         Tip: Group conversations aren’t threaded.
+                    </span>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="bg-white ">
+        <div class=" mx-auto  container flex   pl-60  pt-16 items-start">
+            <div class="px-2 py-3 leading-8 ">
+                    <p class="text-[19px] font-bold block">
+                        Message History
+                    </p>
+                    <span class="mt-2 block text-[20px] font-thin" >
+                        Collaborate on long-term projects with teams or specific groups. 
+                    </span>
+                    <span class="mt-2 block text-[20px] font-thin" >
+                        History is on by default and kept based on your organization’s policy. 
+                    </span>
+                    <span class="mt-2 block text-[20px] font-thin" >
+                        If you use a work or school Google Account, the administrator may turn <br> history on or off. 
+                    </span>
+                    <span class="mt-2 block text-[20px] font-thin" >
+                        When history is:
+                    </span>
+                    <ul class="mt-[6px]   list-square pl-5">
+                        <li class=" text-[20px] font-thin"  >
+                            On: Messages follow your organization's data retention policy. If you use <br> a personal account, messages are kept indefinitely.
+                        </li>
+                        <li class=" text-[20px] font-thin" >
+                            Off: Messages are removed after 24 hours.
+                        </li>
+                    </ul>
+            </div>
+            <div class=" ml-4">
+                <img class="px-3 w-full h-[224px] bg-cover" src="https://lh5.googleusercontent.com/8S0rXnScQ7HGYer8yJmENj4wUObNUpjQ098c9U00fFro7XLSlg9Dk1SNZLYu5HWRbZy02Jz2CE3e-jxsBDGt75KaO9fl0-NV9mEsQib7Oqh7D7XDvwDnOquDBEpweJIJBg=w1280" alt="">
+                <div class="px-2 py-3 text-center " >
+                    <span class=" !leading-6 ">
+                        Picture caption option with a short <br> description of image
                     </span>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+import { text } from '@fortawesome/fontawesome-svg-core';
+import { ref } from 'vue';
+
+
+</script>
--- src/pages/PageClass.vue
+++ src/pages/PageClass.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        
+        <div class=" w-full h-[370px] -z-10 bg-cover bg- bg-center bg-no-repeat  bg-[url('https://lh3.googleusercontent.com/BiKM-I5SPHKhZR9lIRlbaYA0iQ7EyVRQfV75HId7O5q_ibCdVoideRcLCrEKJjeo3dNacjcwPNcGHSKyCEfCx8U=w16383')]"></div>
+        <div class="py-4">
+            <div class="mx-auto px-9">
+                <div class="py-3">
+                    <div class="h-11"></div>
+                    <div class="px-2 py-3 flex">
+                        <div>
+                            <font-awesome-icon class="text-3xl" :icon="['fas', 'book']" />
+                            <span class="">
+                                Wiki/Page
+                            </span>
+                        </div>
+                        <div>Have you explored Google Spaces for internal communications?</div>
+                        <div>Set up your teams for success and foster collaboration.</div>
+                        <div class="h-7"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
